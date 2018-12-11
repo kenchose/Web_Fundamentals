183e2ee2d492a77fa90766913ef59511833e8b7c
Kenny Pham <kennympham@gmail.com>
2018-12-11 01:24:26 -0800

    Delete range_print.js

--- Javascript/range_print.js
+++
@@ -1,6 +0,0 @@
-// Create a function that can take a start point, end point, and skip amount, to print all numbers in the range.
-function rangePoint (start, end, amt) {
-    for(var i = start; i < end; i += amt) {
-        console.log(i);
-    }
-}
