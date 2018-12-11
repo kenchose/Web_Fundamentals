51e220d6938f7eb27ce74ea8a61ebe66babb1842
Kenny Pham <kennympham@gmail.com>
2018-12-11 01:01:06 -0800

    Add files via upload
    Initial commit

---
+++ Javascript/Foundation Concepts/I Do Declare.js
@@ -0,0 +1,22 @@
+// Declare variables of the following types:
+// 4 Numbers
+// 3 Strings
+// 2 Booleans
+// 1 Undefined Variable
+// Give the variables descriptive names (ex: string1 and string2)
+// Print each one to the console with a description (ex: console.log("The First String:", string1);)
+// Try to be creative! (don't use 1, 2, 3, and 4 for your numbers -- test out negatives or decimals!)
+
+var num1 = 1 + 2;
+var num2 = num1 * 2;
+var num3 = num1 * num2;
+if (num1 % num3 !== 0) {
+    var num4 = -23;
+} else {num4 = 0}
+var thisIsTrue = true;
+var thisIsFalse = false;
+var notDerfined;
+var string1 = 'My first name';
+console.log('My first name', string1);
+var string2 = 'My second name';
+console.log('My second name', string2);
